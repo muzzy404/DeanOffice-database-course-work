f511a82aa55b182fdc00bad35acebf1b88430a92
Daria Zelenova <muzzy404@gmail.com>
2017-12-07 02:03:39 +0300

    constats class

--- DeanOfficeDatabase.pro
+++ DeanOfficeDatabase.pro
@@ -30,7 +30,8 @@ SOURCES += \
         mainwindow.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+    constants.h
 
 FORMS += \
         mainwindow.ui
---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <QString>
+
+class Constants {
+  static constexpr QString ADMIN_LOGIN     = "DeanOfficeAdmin";
+  static constexpr QString TEACHER_LOGIN   = "DeanOfficeTeacher";
+  static constexpr QString SECRETARY_LOGIN = "DeanOfficeSecretary";
+}
+
+
+#endif // CONSTANTS_H
