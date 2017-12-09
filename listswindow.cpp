1bf35c4e277a051e004ac4aedba595fda7be4dd2
Daria Zelenova <muzzy404@gmail.com>
2017-12-09 03:06:22 +0300

    firs select for students table

--- listswindow.cpp
+++ listswindow.cpp
@@ -1,12 +1,22 @@
 #include "listswindow.h"
 #include "ui_listswindow.h"
 
+#include <QSqlTableModel>
+
 ListsWindow::ListsWindow(std::shared_ptr<QSqlDatabase> database, QWidget * parent) :
   QWidget(parent),
   ui(new Ui::ListsWindow),
   db(database)
 {
   ui->setupUi(this);
+
+  QSqlTableModel * model = new QSqlTableModel(parent, *db);
+
+  model->setTable("Students");
+  model->select();
+
+  ui->tableViewDataSpace->setModel(model);
+  ui->tableViewDataSpace->show();
 }
 
 ListsWindow::~ListsWindow()
