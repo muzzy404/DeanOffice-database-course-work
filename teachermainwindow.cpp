dcb6a2aac7a15ba6ece9effca506f87948b928e7
Daria Zelenova <muzzy404@gmail.com>
2017-12-14 03:47:51 +0300

    teacher window design fix

--- teachermainwindow.cpp
+++ teachermainwindow.cpp
@@ -1,12 +1,18 @@
 #include "teachermainwindow.h"
 #include "ui_teachermainwindow.h"
 
+#include "projections.h"
+
 TeacherMainWindow::TeacherMainWindow(std::shared_ptr<QSqlDatabase> database, QWidget * parent) :
   QWidget(parent),
   ui(new Ui::TeacherMainWindow),
   db(database)
 {
   ui->setupUi(this);
+
+  Projections::updateAll(*db);
+
+  ui->tchComBoxDep->addItems(Projections::getDepartmentsList());
 }
 
 TeacherMainWindow::~TeacherMainWindow()
